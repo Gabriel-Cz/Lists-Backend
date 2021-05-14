6d2ca7550648083ca2f939fb14b9013b06893db9
Gabriel Cazares <gabrielczhz@gmail.com>
2021-05-14 00:25:28 -0500

    enable cors origin header

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ const corsOptions = {
 }
 
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(morgan('tiny'));
 app.use(express.json());
 app.use(express.urlencoded( { extended: true, } ));
