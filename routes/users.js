aab7a660d3324cabf1c1dc58184bdac112fc02d0
Gabriel Cazares <gabrielczhz@gmail.com>
2021-05-21 01:04:17 -0500

    some details in auth

--- routes/users.js
+++ routes/users.js
@@ -1,39 +1,38 @@
 import express from 'express'
-const router = express.Router();
-
+import bcrypt from 'bcrypt'
 import User from '../models/user'
 
-// Hash for the password
-const bcrypt = require('bcrypt');
 const _ = require('underscore');
+const router = express.Router();
  
 const { verificateAuth } = require('../middlewares/auth'); 
 const saltRound = 10;
 
 router.post('/new-user', async(req, res) => {
 
-    const body = {
-        name: req.body.name,
-        email: req.body.email,
-    }
-
-    body.password = bcrypt.hashSync(req.body.password, saltRound);
-    
+    const body = _.pick(req.body, ['name', 'email', 'password'])
     try {
         const userDB = await User.findOne({email: body.email});
         if(userDB) {
             return res.status(401).json({
                 message: "El email ya se encuentra registrado, Inicia Sesion."
             });
+        } else if (body.password !== '') {
+            body.password = bcrypt.hashSync(req.body.password, saltRound);
+            const userDB = await User.create(body);
+            return res.status(200).json(userDB);
         } else {
             const userDB = await User.create(body);
-            res.status(200).json(userDB);
+            return res.status(400).json({
+                message: "La contraseña es necesaria."
+            });
         }
     }
 
     catch (err) {
+        console.log(err);
         return res.status(500).json({
-            message: "No se pudo registrar el usuario, intenta de nuevo",
+            message: 'La contraseña es necesaria.',
         })
     }
 });
